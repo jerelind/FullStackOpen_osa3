0d787a1f00625977fc2883ae51df50b23b6287bf
Jere Lindholm <lindholmjere@gmail.com>
2018-12-14 12:21:56 +0200

    App

--- modules/person.js
+++ modules/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://jerelind:jere7158#@ds151892.mlab.com:51892/puhelinmuistio'
+const url = 'mongodb://notes:jere7158#@ds151892.mlab.com:51892/puhelinmuistio'
 
 mongoose.connect(url)
 
